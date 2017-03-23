5b3e72ca91a2a39b2e23a456777724251f41f752
@dezmathio <janjos@weddingwire.com>
2017-03-23 11:07:23 -0400

    fix deps and add more slaves

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,26 @@
 use Mix.Config
 
-config :blitzy, master_node: :"a@127.0.0.1"
+config :blitzy, master_node: :"dezmathio@127.0.0.1"
 
-config :blitzy, slave_nodes: [:"b@127.0.0.1",
-                              :"c@127.0.0.1",
-                              :"d@127.0.0.1"]
+config :blitzy, slave_nodes: [:"dez-slave001@127.0.0.1",
+                              :"dez-slave002@127.0.0.1",
+                              :"dez-slave003@127.0.0.1",
+                              :"dez-slave004@127.0.0.1",
+                              :"dez-slave005@127.0.0.1",
+                              :"dez-slave006@127.0.0.1",
+                              :"dez-slave007@127.0.0.1",
+                              :"dez-slave008@127.0.0.1",
+                              :"dez-slave009@127.0.0.1",
+                              :"dez-slave010@127.0.0.1",
+                              :"dez-slave011@127.0.0.1",
+                              :"dez-slave012@127.0.0.1",
+                              :"dez-slave013@127.0.0.1",
+                              :"dez-slave014@127.0.0.1",
+                              :"dez-slave015@127.0.0.1",
+                              :"dez-slave016@127.0.0.1",
+                              :"dez-slave017@127.0.0.1",
+                              :"dez-slave018@127.0.0.1",
+                              :"dez-slave019@127.0.0.1",
+                              :"dez-slave020@127.0.0.1"]
 
 
